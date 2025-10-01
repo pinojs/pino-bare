061b08e8f6e62a3563220d2819e247f7787def97
Yasser Nascimento <dev@yasser.email>
2025-10-01 09:54:44 -0300

    Add globals to worker threads

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 require('bare-process/global')
 
+const Worker = require('bare-worker', { with: { imports: './imports.json' } })
+const { resolve } = require('bare-path')
+
+Worker.preload(resolve('worker-preload'))
+
 module.exports = require('pino', { with: { imports: './imports.json' } })
